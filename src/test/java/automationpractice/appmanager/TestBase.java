51e07a1d55c346b3ae719a64f8b5836b52628db2
OO <murashkina.ea@gmail.com>
2019-02-11 12:54:10 +0300

    browsers change2

--- src/test/java/automationpractice/appmanager/TestBase.java
+++ src/test/java/automationpractice/appmanager/TestBase.java
@@ -2,10 +2,11 @@ package automationpractice.appmanager;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.remote.BrowserType;
 
 public class TestBase {
 
-    public ApplicationManager app = new ApplicationManager();
+    public ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeMethod
     public void setUp() {
